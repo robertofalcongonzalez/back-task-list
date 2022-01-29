a7e6c59c63354e84ce04bda0ca8bc38cf30cf6e5
Roberto <robertofalcongonzalez@gmail.com>
2022-01-29 01:47:57 -0500

    fixing connection errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksModule } from './tasks/tasks.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TasksModule } from "./tasks/tasks.module";
 import { TasksEntity } from "./tasks/tasks.entity";
 
 @Module({
   imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'ec2-52-31-219-113.eu-west-1.compute.amazonaws.com',
-    username: 'lwvfzsyjiajptr',
-    password: '5432',
-    database: 'd7uem1ikfsshv7',
+    name: 'default',
+    type: "postgres",
+    host: "ec2-52-31-219-113.eu-west-1.compute.amazonaws.com",
+    port: 5432,
+    username: "lwvfzsyjiajptr",
+    password: "fb57cf751022abf51680215d6830934d2e592aa2165dda1cb30a79211fa22c00",
+    database: "d7uem1ikfsshv7",
+    synchronize: true,
+    ssl: true,
     entities: [
-      TasksEntity,
-    ],
-    synchronize: false,
+      TasksEntity
+    ]
   }), TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
