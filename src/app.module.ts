b0b6ac0e203e9b2b204cda0b521d9fca42a574da
Roberto <robertofalcongonzalez@gmail.com>
2022-02-26 09:48:54 -0500

    Updating Database User and Password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,32 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { TasksModule } from "./tasks/tasks.module";
-import { TasksEntity } from "./tasks/tasks.entity";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksEntity } from './tasks/tasks.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    name: 'default',
-    type: "postgres",
-    host: "ec2-52-31-219-113.eu-west-1.compute.amazonaws.com",
-    port: 5432,
-    username: "lwvfzsyjiajptr",
-    password: "fb57cf751022abf51680215d6830934d2e592aa2165dda1cb30a79211fa22c00",
-    database: "d7uem1ikfsshv7",
-    synchronize: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      }
-    },
-    entities: [
-      TasksEntity
-    ]
-  }), TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'postgres',
+      host: 'ec2-54-194-147-61.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'tuhpkvrjczjbtc',
+      password:
+        'c171a015d8a8c5e2a8ddab3129f3f01f866bbf137a35ed668f3580d2973ba63b',
+      database: 'd2episr2bal8jl',
+      synchronize: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      entities: [TasksEntity],
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
