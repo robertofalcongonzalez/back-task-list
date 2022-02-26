30bca3396327b42083f32d8e2041ebc48441715d
Roberto <robertofalcongonzalez@gmail.com>
2022-02-26 09:42:08 -0500

    Fixing CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
